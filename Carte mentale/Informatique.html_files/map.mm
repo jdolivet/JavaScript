37287bad5a75688ebafc1b55ac1e9281477b8e39
jdolivet <jdolivet@hotmail.com>
2016-09-26 09:30:21 -0300

    Add files via upload

---
+++ Carte mentale/Informatique.html_files/map.mm
@@ -0,0 +1,161 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1402064525805" ID="ID_1921388611" MODIFIED="1403114565784" STYLE="bubble" TEXT="Informatique">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1402064750291" HGAP="43" ID="ID_221624129" MODIFIED="1403114637133" POSITION="right" TEXT="Algorithmique" VSHIFT="-65">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1402064852374" HGAP="30" ID="ID_567062514" MODIFIED="1403114510333" TEXT="Conception" VSHIFT="-22">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1402067363334" ID="ID_77124065" MODIFIED="1402067371521" TEXT="Validit&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1402067377909" ID="ID_867144649" MODIFIED="1402067385845" TEXT="Complexit&#xe9;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1402065128048" HGAP="145" ID="ID_763073483" MODIFIED="1403114506605" TEXT="Structures lin&#xe9;aires" VSHIFT="3">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1402065139524" HGAP="139" ID="ID_1799967774" MODIFIED="1403114504301" TEXT="Graphes et arbres" VSHIFT="-33">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1402065157384" HGAP="86" ID="ID_716938858" MODIFIED="1403114513093" TEXT="Dichotomie" VSHIFT="6">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1402065187112" HGAP="87" ID="ID_609812668" MODIFIED="1403114516125" TEXT="Algorithmes classiques" VSHIFT="-9">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1402747970575" ID="ID_1212309817" MODIFIED="1402747991804" TEXT="Algorithmes de tri">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1402065169250" HGAP="99" ID="ID_542571191" MODIFIED="1403114520917" TEXT="R&#xe9;cursivit&#xe9;" VSHIFT="-33">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1402064775927" HGAP="66" ID="ID_1192172293" MODIFIED="1403114632694" POSITION="right" STYLE="bubble" TEXT="Architecture" VSHIFT="-20">
+<edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403114316997" ID="ID_219093928" MODIFIED="1403114330462" TEXT="Op&#xe9;rations bool&#xe9;ennes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403114394873" ID="ID_16773111" MODIFIED="1403114399950" TEXT="Portes logiques">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1403114403680" ID="ID_1129935585" MODIFIED="1403114432447" TEXT="Circuits s&#xe9;quentiels"/>
+<node COLOR="#111111" CREATED="1403114414102" ID="ID_1993883078" MODIFIED="1403114422371" TEXT="Circuits combinatoires"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1403114457534" ID="ID_1460039963" MODIFIED="1403114476080" TEXT="Organisation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403114478750" ID="ID_422633819" MODIFIED="1403114488122" TEXT="Circuit de Von Neumann">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1402064765232" HGAP="108" ID="ID_1293921896" MODIFIED="1403114605990" POSITION="left" TEXT="Programmation" VSHIFT="-53">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1402065312825" HGAP="100" ID="ID_338182475" MODIFIED="1403114593926" TEXT="Machine de Turing" VSHIFT="25">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1402065321233" HGAP="26" ID="ID_1504332902" MODIFIED="1402748052718" TEXT="Machine de Turing virtuelle" VSHIFT="4">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1403114038329" HGAP="98" ID="ID_68260476" MODIFIED="1403114076714" TEXT="Concepts">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1402065283305" HGAP="84" ID="ID_1446554174" MODIFIED="1403114080020" TEXT="Langages de programmation" VSHIFT="-2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1402065294753" ID="ID_1463825174" MODIFIED="1402066023569" TEXT="Langage Python">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1402065304651" ID="ID_579940570" MODIFIED="1402066023569" TEXT="Langage HTML">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1402067213339" HGAP="260" ID="ID_1843167152" MODIFIED="1403114581326" TEXT="Cryptographie" VSHIFT="-9">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1402074421046" ID="ID_396527578" MODIFIED="1402748068492" TEXT="Exemple">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403114140862" ID="ID_952036432" MODIFIED="1403114145063" TEXT="Hill">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403114151101" ID="ID_1297002606" MODIFIED="1403114154243" TEXT="RSA">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1402067232868" HGAP="205" ID="ID_1199834142" MODIFIED="1403114600727" TEXT="Traitement de l&apos;image" VSHIFT="4">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1402065213912" HGAP="94" ID="ID_1846071486" MODIFIED="1403114598414" TEXT="Arithm&#xe9;tique flottante" VSHIFT="-39">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403114548486" ID="ID_1722707376" MODIFIED="1403114565784" TEXT="Analyse num&#xe9;rique">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1402064788814" HGAP="120" ID="ID_950529283" MODIFIED="1403114610894" POSITION="left" TEXT="Information" VSHIFT="-4">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1402065349512" ID="ID_496478039" MODIFIED="1402747950335" TEXT="Repr&#xe9;sentation des nombres">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1402065364042" ID="ID_374811388" MODIFIED="1402066023570" TEXT="Num&#xe9;risation des caract&#xe8;res">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1402065390532" ID="ID_1764447774" MODIFIED="1402066023570" TEXT="Num&#xe9;risation des images">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1402065401705" ID="ID_725019235" MODIFIED="1402066023570" TEXT="Num&#xe9;risation des sons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1402065449153" HGAP="42" ID="ID_1902153315" MODIFIED="1403114619406" TEXT="Structuration de l&apos;information" VSHIFT="-14">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1402065487150" HGAP="59" ID="ID_1793170430" MODIFIED="1402066023570" TEXT="Les fichiers" VSHIFT="33">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1402065462637" HGAP="95" ID="ID_1684950137" MODIFIED="1403114617221" TEXT="Contr&#xf4;le de l&apos;information" VSHIFT="-23">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403114187397" ID="ID_440491772" MODIFIED="1403114191902" TEXT="Bases de donn&#xe9;es">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1402748163832" HGAP="25" ID="ID_64561136" MODIFIED="1403114634631" POSITION="right" TEXT="R&#xe9;seaux" VSHIFT="45">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1402748181204" ID="ID_1483803721" MODIFIED="1403114237721" TEXT="Typologie">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403114240885" ID="ID_1741805090" MODIFIED="1403114249539" TEXT="Structure">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403114253045" ID="ID_686883684" MODIFIED="1403114259287" TEXT="Internet">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
